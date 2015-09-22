0f106d4dcccdad6c101381fcfe6dd5c41fe8e505
zibs <elizibin@gmail.com>
2015-09-22 12:09:49 -0700

    change config

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Project2055
     # config.i18n.default_locale = :de
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
